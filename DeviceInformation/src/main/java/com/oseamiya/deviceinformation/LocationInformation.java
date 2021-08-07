7e7ffb9b5bd0745acb4ae83a831c540f6ae58799
Oseamiya <85782575+oseamiya@users.noreply.github.com>
2021-08-07 14:09:18 +0545

    Fixed Invoke Virtual & latitude 0 for first time
    In this commit, I've fixed errors like invoke virtual method on a null object refrence caused because of passing null variable and fixed showing 0 latitude first time while giving permissions and shortened the code by adding a method.

--- DeviceInformation/src/main/java/com/oseamiya/deviceinformation/LocationInformation.java
+++ DeviceInformation/src/main/java/com/oseamiya/deviceinformation/LocationInformation.java
@@ -7,6 +7,7 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.app.Activity;
 
 import androidx.core.app.ActivityCompat;
 
@@ -16,28 +17,36 @@ import java.util.Locale;
 
 public class LocationInformation {
     private final Context context;
-
+    private final Activity activity;
     public LocationInformation(Context context) {
         this.context = context;
+        this.activity = (Activity) context;
     }
-
+    public Location getLocation() {
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this.context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this.activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location == null) {
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }else if(location == null){
+            location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+        }
+        return location;
+    }
     public  double getCurrentLatitude() {
-        LocationManager lm = (LocationManager) this.context.getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this.context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            /* TODO */
+        if (getLocation() == null) {
+            return 0;
         }
-        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        return location.getLatitude();
-
+        return getLocation().getLatitude();
     }
 
-    public  double getCurrentLongitude(){
-        LocationManager lm = (LocationManager) this.context.getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this.context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            /* TODO */
+    public double getCurrentLongitude() {
+        if (getLocation() == null) {
+            return 0;
         }
-        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        return location.getLongitude();
+        return getLocation().getLongitude();
     }
 
     public String getStreetAddress(double latitude , double longitude) {
